52ca73c19ee00928d3be06ea163b59b31dfab6fd
Tatiana Mraz <tatiana.mrazova@icloud.com>
2022-12-22 12:53:52 +0100

    *ngIf - navigation through structural directive

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  loadedFeature = '';
+
+  onNavigate(feature: string) {
+    // take feature emitted out of header component and assign the value to the loadedFeature
+    this.loadedFeature = feature;
+  }
 }
