732ea940e1d4e4d8262541a9c6f884b50679f26f
Tatiana Mraz <tatiana.mrazova@icloud.com>
2022-12-22 09:12:42 +0100

    model.ts creating

--- app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('test recipe', 'recipe description', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fvectors%2Frecipe-label-icon-symbol-spoon-575434%2F&psig=AOvVaw2Kus5UqdImVSGkSzTtRg5l&ust=1671780917964000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCLDog7XbjPwCFQAAAAAdAAAAABAD')
+  ];
 
   constructor() { }
 
