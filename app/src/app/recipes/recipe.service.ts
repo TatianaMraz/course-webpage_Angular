f0a8e81fe3f2464f00c003549cdcf58dce6b7a21
Tatiana Mraz <tatiana.mrazova@icloud.com>
2022-12-23 18:52:15 +0100

    service for cross-component communication

--- app/src/app/recipes/recipe.service.ts
+++ app/src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
+import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
+
     private recipes: Recipe[] = [
         new Recipe('first recipe', 'recipe description', '../../../assets/img/1.png'),
         new Recipe('second recipe', 'recipe description', '../../../assets/img/2.png')
